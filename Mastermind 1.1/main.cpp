0fea748c90871c479e65f91416716c6302aa1145
Jet <Jet@jet.local>
2017-04-18 16:16:54 -0700

    Initial Commit

---
+++ Mastermind 1.1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Mastermind 1.1
+//
+//  Created by Jet on 4/18/17.
+//  Copyright © 2017 Jet. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
